a26711b3b8b0a53c01f79e1f8348c4d2e438446a
junglegap1 <nicholas.rusli@gmail.com>
2025-06-26 16:08:52 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,16 +5,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyAqkcQyUzvWC8BuSYGY54QBjaRLl18urcM",
   authDomain: "pyramid3s.firebaseapp.com",
   projectId: "pyramid3s",
-  storageBucket: "pyramid3s.firebasestorage.app",
+  storageBucket: "", // empty since no storage allowed
   messagingSenderId: "553574468724",
   appId: "1:553574468724:web:397b7fe78610f319685521"
 };
 
-// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
-// Export both so you can import them later
 export { app, db };
