188bd0d372d729617587e42961800ed189a07173
junglegap1 <nicholas.rusli@gmail.com>
2025-06-26 15:55:50 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,20 @@
 // src/firebase.ts
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";  // import storage service if you want to use storage
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAqkcQyUzvWC8BuSYGY54QBjaRLl18urcM",
+  authDomain: "pyramid3s.firebaseapp.com",
+  projectId: "pyramid3s",
+  storageBucket: "pyramid3s.firebasestorage.app",
+  messagingSenderId: "553574468724",
+  appId: "1:553574468724:web:397b7fe78610f319685521"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+
+// Initialize Firebase Storage and export it
+const storage = getStorage(app);
+
+export { app, storage };
